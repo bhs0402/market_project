91455194b997ce2aec7c2905e3f3e6ff6ea05000
bhs <ghdtjq0118@gmail.com>
2024-12-31 17:10:03 +0900

    보완

--- src/main/java/com/bhs/sssss/controllers/AdminController.java
+++ src/main/java/com/bhs/sssss/controllers/AdminController.java
@@ -1,12 +1,23 @@
 package com.bhs.sssss.controllers;
 
+import com.bhs.sssss.entities.MemberEntity;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller(value = "/admin")
+@Controller
+@RequestMapping(value = "/admin")
 public class AdminController {
 
-
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getAdmin(@SessionAttribute(value = "member", required = false) MemberEntity member) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("member", member);
+        modelAndView.setViewName("/admin/admin");
+        return modelAndView;
+    }
 }
