58371e33e72af6b53c38a64584e7a73d51b4e0c8
bhs <ghdtjq0118@gmail.com>
2025-01-03 14:09:06 +0900

    test

--- src/main/resources/static/admin/assets/scripts/member-management.js
+++
@@ -1,3 +0,0 @@
-const $detailButton = Array.from(document.querySelectorAll('#main > .right > .content-container > .content-box > .member-container > .member-list > .member-table > tbody > tr > .detail > .detail-button'));
-
-
